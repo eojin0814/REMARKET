3c57308ae0da6519d64d6d3e57316a441c24a718
yujindonut <bbb3631@naver.com>
2023-04-12 23:42:36 +0900

    feat: UserController 생성

---
+++ src/main/java/com/softwareapplication/remarket/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.softwareapplication.remarket.controller;
+
+import com.softwareapplication.remarket.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+}
