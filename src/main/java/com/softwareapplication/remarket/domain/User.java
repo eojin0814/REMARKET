f1ed54c52f1a6ad39104221dfde6c6156424d607
yujindonut <bbb3631@naver.com>
2023-04-28 18:56:36 +0900

    chore: User Entity 수정

--- src/main/java/com/softwareapplication/remarket/domain/User.java
+++ src/main/java/com/softwareapplication/remarket/domain/User.java
@@ -1,37 +1,55 @@
 package com.softwareapplication.remarket.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 
 @Builder
-@Getter @Setter
+@Getter
+@Setter
 @DynamicInsert
-@Entity
+@Entity(name = "Member")
 @NoArgsConstructor
 @AllArgsConstructor
-public class User extends BaseEntity{
+public class User {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  @Column(name = "user_id")
-  private Long id;
-  @Column(nullable = false)
-  private String email;
-  @Column(nullable = false)
-  private String password;
-  private String name;
-  private String phone;
-  @Column(name = "nickname")
-  private String nickName;
-  private String address;
-  private String profileImg;
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String email;
+
+    @Column(nullable = false, length = 16)
+    private String password;
+
+    @Column(unique = true, nullable = false, length = 10)
+    private String name;
+
+    @Column(length = 11)
+    private String phone;
+
+    @Column(length = 50)
+    private String address;
+
+    private String imageUrl;
+
+    public boolean matchPassword(String newPassword) {
+        return newPassword.equals(password);
+    }
+
+    public void updateUser(String name, String phone,
+                           String address) {
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+    public void updateProfileImage(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
 }
