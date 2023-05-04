e5328e20adc9c9104bb6ebf4a72f4dcb922bac84
정재운 <jaeun101501@gmail.com>
2023-05-04 23:27:22 +0900

    feat: GroupApply entity 생성

---
+++ src/main/java/com/softwareapplication/remarket/domain/GroupApply.java
@@ -0,0 +1,36 @@
+package com.softwareapplication.remarket.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+
+@Builder
+@Getter
+@Setter
+@DynamicInsert
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupApply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "groupApply_id")
+    private Long id; //공동 구매 신청 id
+
+    @Column(nullable = false)
+    private String address; //주소
+
+    @Column(nullable = false)
+    private Integer count; //신청 개수
+
+    @Column(nullable = false)
+    private String phone; //전화번호
+
+    @Column(name = "group_id", nullable = false)
+    private Long groupId; //공동구매 게시글 id
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId; //구매자(신청자) id
+
+}
