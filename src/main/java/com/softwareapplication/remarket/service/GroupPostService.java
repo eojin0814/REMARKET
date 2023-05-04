65c9a83184fb6590b392cbea1051218dc03cdc1e
정재운 <jaeun101501@gmail.com>
2023-05-04 23:07:04 +0900

    feat: 공동구매 service 생성

---
+++ src/main/java/com/softwareapplication/remarket/service/GroupPostService.java
@@ -0,0 +1,45 @@
+package com.softwareapplication.remarket.service;
+
+import com.softwareapplication.remarket.domain.GroupPost;
+import com.softwareapplication.remarket.dto.GroupPostDto;
+import com.softwareapplication.remarket.repository.GroupPostRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class GroupPostService {
+
+    private GroupPostRepository groupPostRepository;
+
+    public GroupPostService(GroupPostRepository groupPostRepository){
+        this.groupPostRepository = groupPostRepository;
+    }
+    @Transactional
+    public Long savePost(GroupPostDto groupPostDto){
+        return groupPostRepository.save(groupPostDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<GroupPostDto> getGroupPostList(){
+        List<GroupPost> groupPostList = groupPostRepository.findAll();
+        List<GroupPostDto> groupPostDtoList = new ArrayList<>();
+        for(GroupPost groupPost : groupPostList){
+            GroupPostDto groupPostDto = GroupPostDto.builder()
+                    .id(groupPost.getId())
+                    .title(groupPost.getTitle())
+                    .dueDate(groupPost.getDueDate())
+                    .product(groupPost.getProduct())
+                    .content(groupPost.getContent())
+                    .numPeople(groupPost.getNumPeople())
+                    .price(groupPost.getPrice())
+                    .image(groupPost.getImage())
+                    .userId(groupPost.getUserId())
+                    .build();
+            groupPostDtoList.add(groupPostDto);
+        }
+        return groupPostDtoList;
+    }
+}
