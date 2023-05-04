8152a74be248f772e4391bd040da2ed47da9df36
yujindonut <bbb3631@naver.com>
2023-05-04 13:03:17 +0900

    chore: 파일명 수정

---
+++ src/main/java/com/softwareapplication/remarket/controller/MainController.java
@@ -0,0 +1,28 @@
+package com.softwareapplication.remarket.controller;
+
+import com.softwareapplication.remarket.domain.User;
+import com.softwareapplication.remarket.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
+public class MainController {
+
+    private final UserService userService;
+    @GetMapping()
+    public String home(Model model, @SessionAttribute(name = "email", required = false) String email) {
+
+        User loginUser = userService.getLoginUserByEmail(email);
+
+        if(loginUser != null) {
+            model.addAttribute("email", loginUser.getEmail());
+        }
+        return "layout/default";
+    }
+}
