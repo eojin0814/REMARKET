4c5b5c79ed5bcf799c75d0344c0e2dcfc7aa81e2
정재운 <jaeun101501@gmail.com>
2023-05-07 07:13:41 +0900

    chore: 생성, 수정, 삭제 controller 추가

--- src/main/java/com/softwareapplication/remarket/controller/GroupPostController.java
+++ src/main/java/com/softwareapplication/remarket/controller/GroupPostController.java
@@ -1,14 +1,18 @@
 package com.softwareapplication.remarket.controller;
 
+import com.softwareapplication.remarket.domain.User;
 import com.softwareapplication.remarket.dto.GroupPostDto;
+import com.softwareapplication.remarket.dto.UserDto;
 import com.softwareapplication.remarket.service.GroupPostService;
+import com.softwareapplication.remarket.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -18,33 +22,71 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GroupPostController {
 
-    private GroupPostService groupPostService;
-    public GroupPostController(GroupPostService groupPostService){
-        this.groupPostService = groupPostService;
-    }
-
-//    @GetMapping("/groupList")
-//    public String groupList(Model model) {
-//        List<GroupPostDto> groupPostDtoList = groupPostService.getGroupPostList();
-//        if(groupPostDtoList != null) {
-//            model.addAttribute("groupDtoList", groupPostDtoList);
-//        }
-//        return "group/groupList";
-//    }
+    private final GroupPostService groupPostService;
+    private final UserService userService;
     @GetMapping("/groupList")
-    public String groupList() {
-        return "group/groupList";
+    public ModelAndView groupList() {
+        ModelAndView mav = new ModelAndView("group/groupList");
+        mav.addObject("gList", groupPostService.getGroupPostList());
+        return mav;
     }
 
     @GetMapping("/create")
-    public String createPost(Model model, GroupPostDto groupPostDto){
+    public String createPost(HttpServletRequest httpServletRequest, Model model, GroupPostDto groupPostDto){
+        HttpSession session = httpServletRequest.getSession();
+        String email = (String)session.getAttribute("email");
+        User user = userService.getLoginUserByEmail(email);
+        groupPostDto.setUserId(user.getUserId());
         model.addAttribute("groupPostDto", groupPostDto);
         return "group/createGroupPost";
     }
 
     @PostMapping("/create")
-    public String savePost(GroupPostDto groupPostDto){
+    public String savePost(@Valid GroupPostDto groupPostDto, BindingResult result){
+        if(result.hasErrors()){
+            return "group/createGroupPost";
+        }
         groupPostService.savePost(groupPostDto);
+        return "redirect:/group/groupList";
+    }
+
+    @GetMapping("/detail")
+    public ModelAndView detailPost(@RequestParam("id")Long id, HttpServletRequest httpServletRequest){
+        GroupPostDto groupPostDto = groupPostService.findPost(id);
+        UserDto.Info postUser = userService.getUserByUserId(groupPostDto.getUserId());
+
+        HttpSession session = httpServletRequest.getSession();
+        String email = (String)session.getAttribute("email");
+        User currentUser = userService.getLoginUserByEmail(email);
+
+        ModelAndView mav = new ModelAndView("group/detailGroupPost");
+        mav.addObject("groupPostDto", groupPostDto);
+        mav.addObject("postUser", postUser);
+        mav.addObject("currentUser", currentUser);
+
+        return mav;
+    }
+
+    @GetMapping("/update")
+    public ModelAndView updatePost(@RequestParam("id")Long id){
+        ModelAndView mav = new ModelAndView("/group/updateGroupPost");
+        GroupPostDto groupPostDto = groupPostService.findPost(id);
+        mav.addObject("groupPostDto", groupPostDto);
+        return mav;
+    }
+
+    @PostMapping("/update")
+    public String updateSavePost(@Valid GroupPostDto groupPostDto, BindingResult result){
+        if(result.hasErrors()){
+            return "group/updateGroupPost";
+        }
+        groupPostService.updatePost(groupPostDto);
+        return "redirect:/group/groupList";
+    }
+
+    @GetMapping("/delete")
+    public String deletePost(@RequestParam("id")Long id){
+        groupPostService.deletePost(id);
         return "redirect:/group/groupList";
     }
 
