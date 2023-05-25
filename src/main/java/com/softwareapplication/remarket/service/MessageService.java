8e9512e3d5fa27c4d1b9ad7c37b3a32d217bfd1e
yujindonut <bbb3631@naver.com>
2023-05-25 19:52:10 +0900

    feat: 채팅 서비스 로직 추가

---
+++ src/main/java/com/softwareapplication/remarket/service/MessageService.java
@@ -0,0 +1,32 @@
+package com.softwareapplication.remarket.service;
+
+import com.softwareapplication.remarket.dto.MessageDto;
+import com.softwareapplication.remarket.dto.SharePostDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface MessageService {
+
+	//쪽지함 생성
+	public Long addRoom(Long postIdx, Long senderIdx);
+	
+	//쪽지 리스트 조회 
+	public List<MessageDto.MessageResponse> getAllMessage(Long roomIdx);
+
+	//쪽지함 조회
+	public List<MessageDto.Info> getAllRoom(Long userIdx);
+	
+	//쪽지함 상세 조회
+	public Long getRoom(Long postIdx, Long senderIdx);
+	
+	//참여한 쪽지의 post 조회
+	public List<SharePostDto.MyPageInfo> getAllMessageBySender(Long userIdx);
+
+	//쪽지 보내기 
+	public void sendMessage(MessageDto.Request req, Long senderIdx, Long roomIdx);
+	
+	//쪽지 읽음 처리
+	public void updateIsRead(Long senderIdx, Long roomIdx);
+}
