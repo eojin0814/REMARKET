c3bb1ff390a8bf51e6ce7335e6ce5bedc7d7dcfe
yujindonut <bbb3631@naver.com>
2023-05-25 19:51:03 +0900

    feat: 나눔 게시판, 메세지 repository 추가

---
+++ src/main/java/com/softwareapplication/remarket/repository/MessageInfoRepository.java
@@ -0,0 +1,29 @@
+package com.softwareapplication.remarket.repository;
+
+import com.softwareapplication.remarket.domain.MessageInfo;
+import com.softwareapplication.remarket.domain.MessageRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface MessageInfoRepository extends JpaRepository<MessageInfo, Long> {
+	//쪽지 내용 불러오기
+	@Query(value = "SELECT m "
+			+ "FROM MessageInfo m "
+			+ "WHERE m.room.roomId = ?1 "
+			+ "ORDER BY m.createdDate ASC")
+	List<MessageInfo> findAllByRoomId(Long roomId);
+
+	@Modifying(clearAutomatically = true)
+	@Query(value = "UPDATE MessageInfo m "
+			+ "SET m.is_read='Y' "
+			+ "WHERE m.sender_id != :sender_id AND m.room_idx = :room_idx", nativeQuery = true)
+	public void updateIsRead(@Param("sender_id")Long senderId
+			, @Param("room_id")Long roomId);
+	
+	
+	public MessageInfo findTop1ByRoomOrderByCreatedDateDesc(MessageRoom room);
+}
