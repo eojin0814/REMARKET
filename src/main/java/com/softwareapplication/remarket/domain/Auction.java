e933a084a7a858e5cdd914d64e66787ab80bd26d
이어진 <leeeojin6@gmail.com>
2023-06-16 00:56:10 +0900

    [feat] Auction Entity

--- src/main/java/com/softwareapplication/remarket/domain/Auction.java
+++ src/main/java/com/softwareapplication/remarket/domain/Auction.java
@@ -1,12 +1,13 @@
 package com.softwareapplication.remarket.domain;
 
-
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -16,43 +17,43 @@ import java.util.Date;
 @NoArgsConstructor
 @Getter
 @Entity
-@Table(name="SecondHand")
+@Table(name="Auction")
 @EntityListeners(AuditingEntityListener.class)
-public class SecondHand {
+public class Auction {
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name = "second_hand_id")
-    private Long secondHandId;
-
-    @Column
-    private String title;
-
-    @Column
-    private Long price;
+    @Column(name = "auction_Id")
+    private Long auctionId;
 
     @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
+
     @LastModifiedDate
     @Column
     private LocalDateTime updatedDate;
-    @Column
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "auction_price")
+    private Long auctionPrice;
+
+    @Column(name = "due_date")
+    private Date dueDate;
+
+    @Column(name = "content")
     private String content;
 
-    @OneToOne
-    @JoinColumn(name="img_id")
-    private Image  image; //이미지 첨부
+    @Column(name = "bid_price")
+    private String bidPrice;
+
+    @Column(name = "status")
+    private String status;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    public void update(String title, Image image, String content){
-        this.title=title;
-        this.image=image;
-        this.content=content;
-    }
-
 }
-//ㄷㅏ음주까지 엔티티 서비스 컨트롤러 할 수 있는 곳까지, 엔티티 다해오기
-// 다음주에는 시스템 설계서 제출하는걸ㄹ로 ppt 만들기
