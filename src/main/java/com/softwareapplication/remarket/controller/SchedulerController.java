c31641c62746f793235991fc7718e22f572c4baf
이어진 <leeeojin6@gmail.com>
2023-06-16 01:44:27 +0900

    [feat] Auction scheduler test

--- src/main/java/com/softwareapplication/remarket/controller/SchedulerController.java
+++ src/main/java/com/softwareapplication/remarket/controller/SchedulerController.java
@@ -4,13 +4,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 @EnableScheduling
 public class SchedulerController {
 
     @Scheduled(fixedDelay=600000)
     public void init() {
-        System.out.println("동작여부 확인");
+        Date date = new Date();
+        System.out.println(date + "동작여부 확인");
     }
 
 }
