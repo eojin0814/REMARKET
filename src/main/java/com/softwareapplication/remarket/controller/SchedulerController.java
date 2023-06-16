bd0d6492fa316b1c2478e11d5d73f6c5826211e4
이어진 <leeeojin6@gmail.com>
2023-06-17 05:52:48 +0900

    [feat] Auction create View

--- src/main/java/com/softwareapplication/remarket/controller/SchedulerController.java
+++ src/main/java/com/softwareapplication/remarket/controller/SchedulerController.java
@@ -2,14 +2,20 @@ package com.softwareapplication.remarket.controller;
 
 import com.softwareapplication.remarket.domain.SecondHand;
 import com.softwareapplication.remarket.domain.TenderPrice;
+import com.softwareapplication.remarket.domain.User;
 import com.softwareapplication.remarket.dto.AuctionDto;
+import com.softwareapplication.remarket.dto.GroupPostDto;
 import com.softwareapplication.remarket.dto.SecondHandDto;
 import com.softwareapplication.remarket.service.SchedulerService;
 import com.softwareapplication.remarket.service.SecondHandService;
+import com.softwareapplication.remarket.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,7 +35,7 @@ public class SchedulerController {
 
     private final SchedulerService schedulerService;
 
-
+    private final UserService userService;
     @Scheduled(fixedDelay=6000000)
     public void init() {
 
@@ -48,5 +54,19 @@ public class SchedulerController {
 
         return schedulerService.save(auctionDto);
         //new ModelAndView("redirect: /secondHand"); //수정 될 가능성 ..
+    }
+    @GetMapping("/create")
+    public String createPost(HttpServletRequest httpServletRequest, Model model, AuctionDto auctionDto){
+        HttpSession session = httpServletRequest.getSession();
+        String email = (String)session.getAttribute("email");
+        User loginUser = userService.getLoginUserByEmail(email);
+
+        auctionDto.setUserId(loginUser.getUserId());
+        model.addAttribute("auctionDto", auctionDto);
+
+        if(loginUser != null) {
+            model.addAttribute("email", loginUser.getEmail());
+        }
+        return "Auction/auctionForm";
     }
 }
