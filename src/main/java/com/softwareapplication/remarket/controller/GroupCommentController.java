0f793a7e7d3d9c24f8e99eb98f8002d9a57b52be
정재운 <jaeun101501@gmail.com>
2023-06-18 06:53:09 +0900

    chore: 공동구매 댓글 대댓글 html, GroupCommentApiController => GroupCommentController로 rename

--- src/main/java/com/softwareapplication/remarket/controller/GroupCommentController.java
+++ src/main/java/com/softwareapplication/remarket/controller/GroupCommentController.java
@@ -7,14 +7,15 @@ import com.softwareapplication.remarket.service.GroupCommentService;
 import jakarta.validation.Valid;
 import lombok.*;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 @RequestMapping("/groupComment")
 @RequiredArgsConstructor
-public class GroupCommentApiController {
+public class GroupCommentController {
     private final GroupCommentService groupCommentService;
     private final UserService userService;
 
